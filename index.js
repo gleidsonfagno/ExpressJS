b2236655daa2dc1e6b85e88440dc989e43ddd5c2
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-04-26 21:53:39 -0300

    Catch-all ivalid routes

--- index.js
+++ index.js
@@ -11,28 +11,50 @@ app.get("/", (req, res) => {
 })
 
 app.use("/user", router)
-app.use(express.json())
+// app.use(express.json())
 
-// Method POST: Create
-app.post("/users", (req, res) => {
-    const {name, email} = req.body
+// // Method POST: Create
+// app.post("/users", (req, res) => {
+//     const {name, email} = req.body
+
+//     res.json({
+//         messege: `User ${name} with email ${email} created successfully`
+//     })
+// })
+
+// // Method PUT: updated
+// app.put("/users/:id", (req, res) => {
+//     const userID = req.params.id
+//     const {name, email} = req.body
+//     res.json({
+//         messege: `User ${userID} updated to ${name}, ${email}`
+//     })
+// })
+
+// // Method DELETE: delete
+// app.delete("/users/:id", (req, res) => {
+//     const useerID = req.params.id
+//     res.json({
+//         messege: `User with ID ${useerID} deleted successfully`
+//     })
+// })
+
+app.get("/things/:name/:id([0-9]{5})", (req, res) => {
+    const { name, id } = req.params;
 
     res.json({
-        messege: `User ${name} with email ${email} created successfully`
-    })
-})
+        id,
+        name
+    });
+});
 
-// Method PUT: updated
-app.put("/users/:id", (req, res) => {
-    const userID = req.params.id
-    const {name, email} = req.body
-    res.json({
-        messege: `User ${userID} updated to ${name}, ${email}`
-    })
-})
+// Catch-all ivalid routes
+app.get("*", (req, res) => {
+    res.send("Sorry, this is an invalid route URL.");
+});
+
 
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
-
