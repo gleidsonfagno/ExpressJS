0617fca4bb559857f64e70dbfefbf271c5260a00
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-04-28 22:06:48 -0300

    feat: Middleware

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ const app = express()
 
 const PORT = 3000
 
+// esse Middleware vai agora somete para a rota /welcome
+app.use("/welcome",(req, res, next) => {
+    console.log("Anew request recived at"+Date.now())
+    next()
+})
+
 // Define a simple route
 app.get("/", (req, res) => {
-    res.send("Hello, World")
+    res.send("Hello, Express")
+})
+
+app.get("/welcome", (req, res) => {
+    res.send("Welcomw to Express App")
 })
 
 app.use("/user", router)
