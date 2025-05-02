238d89c686d3a92be18c120e19bea1c51346b796
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-05-01 22:21:46 -0300

    feat: Templating

--- index.js
+++ index.js
@@ -2,33 +2,18 @@ import express from "express";
 import router from "./route.js";
 
 const app = express();
-
 const PORT = 3000;
 
-// app.use((req, res, next) => {
-//   console.log("Start");
-
-//   res.on("finish", () => {
-//     console.log("End");
-//   });
-//   next();
-// });
-
+// Set EJS as the view engine
+app.set("view engine", "ejs")
 
 
 // Define a simple route
 app.get("/", (req, res) => {
-  res.send("Hello, Express");
+  const userName = "Elloon Musk"
+
+  res.render("index", {userName});
 });
-
-app.get("/error", () => {
-  throw new Error("This is test error")
-})
-
-app.use((err,req,res,next) => {
-  console.error(err.message)
-  res.send("Internal Server Error")
-})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
